54873b416751c7923bad6d6a88b8fbd5dfb122de
black9923 <rlaxotka963@gmail.com>
2022-05-24 13:16:39 +0900

    vue- worldcup

---
+++ final-pjt-front/src/components/WorldcupChoice.vue
@@ -0,0 +1,64 @@
+
+<template>
+  
+  <div v-if="!!movie">
+    <h2>{{ movie.title }}</h2>
+    <v-hover
+      v-slot:default="{ hover }"
+      :disabled="disabled"
+      :value="value"
+      max-height="600px"
+    >
+      <v-img
+        @click="select"
+        :src="imgUrl"
+        alt=""
+        width="400px"
+        height="600px"
+        :elevation="hover ? 12 : 2"
+      ></v-img>
+    </v-hover>
+    <p>줄거리</p>
+    <p>{{ movie.overview.slice(0,200) }}...</p>
+
+    <p>평점</p>
+    <p>{{ movie.vote_average }}</p>
+
+    <p>장르</p>
+    <p>{{ movie.genres }}</p>
+  </div>
+
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  data () {
+    return {
+      movieObject: null,
+      disabled: false,
+      value: false,
+    }
+  },
+  
+  props: {
+    movie:{
+      type: Object,
+      required: false,
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('choiceEvent', true)
+    }
+  },
+  computed: {
+    imgUrl: function () {
+      return "https://image.tmdb.org/t/p/original" + this.movie.poster_path
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- final-pjt-front/src/views/WorldcupView.vue
+++ final-pjt-front/src/views/WorldcupView.vue
@@ -1,30 +1,127 @@
 <template>
-  <div>
+  <v-container class="worldcup">
     <h2>Worldcup</h2>
-    <div class="row row-cols-4 row-cols-md-12 g-4 mt-3 mb-5">
-      <movie-card
-        v-for="movie in worldMovies"
-        :key="movie.id"
-        :movie="movie"
-      ></movie-card>
+
+    <div v-if="!finishFlag">
+      <h1>선택해 주세요</h1>
+      <hr>
+      <div v-if="!left">
+        <v-btn v-if="finalRound" @click="goNext">결승전</v-btn>
+        <v-btn v-else @click="goNext">{{ roundNum }}강 시작하기</v-btn>
+      </div>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="6" aling="center">
+          <worldcup-choice
+            id="left"
+            :movie="left"
+            @choiceEvent="choiceLeft"
+          ></worldcup-choice> 
+        </v-col>
+        <v-col cols="6" aling="center">
+          <worldcup-choice
+            id="right"
+            :movie="right"
+            @choiceEvent="choiceRight"
+          ></worldcup-choice> 
+        </v-col>
+      </v-row>
     </div>
-  </div>
+    
+    <div v-if="finishFlag">
+      <h1>우승!</h1>
+      <hr>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="6">
+          <worldcup-choice
+            id="winner"
+            :movie="left"
+          ></worldcup-choice>
+        </v-col>
+
+      </v-row>
+    </div>
+    <div>
+      <router-link to="/">
+        <button 
+          class="btn btn-secondary" 
+        >
+          BACK
+        </button>
+      </router-link>
+    </div>
+
+  </v-container>
+  
 </template>
 
 <script>
-import MovieCard from '@/components/MovieCard.vue'
+import WorldcupChoice from '@/components/WorldcupChoice.vue'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'worldcupView',
+  data() {
+    return {
+      currentRound: [],
+      nextRound: [],
+      roundNum: 16,
+      left: null,
+      right: null,
+      finishFlag: false,
+      finalRound: false,
+    }
+  },
   components: {
-    MovieCard,
+    WorldcupChoice,
   },
   computed: {
-    ...mapGetters(['movies', 'worldMovies']),
+    ...mapGetters(['worldMovies']),
   },
   methods: {
-    ...mapActions(['fetchMovies', 'fetchWorldcupMovies'])
+    ...mapActions(['fetchMovies', 'fetchWorldcupMovies']),
+    choiceLeft() {
+      this.nextRound.push(this.left)
+      this.left = null
+      this.right = null
+      this.goNext()
+    },
+    choiceRight() {
+      this.nextRound.push(this.right)
+      this.left = null
+      this.right = null
+      this.goNext()
+    },
+    goNext() {
+      if (!this.finishFlag && this.currentRound.length === 0) {
+        this.currentRound = this.worldMovies
+      }
+      this.left = this.currentRound.pop(0)
+      this.right = this.currentRound.pop(0)
+    }
+  },
+  watch: {
+    left: function() {
+      if (this.currentRound.length === 0 && !this.left) {
+        this.currentRound = this.nextRound
+        this.nextRound = []
+        this.roundNum /= 2
+        if (this.roundNum === 2) {
+          this.finalRound = true
+        }
+      }
+    },
+    right: function() {
+      if (this.nextRound.length === 0 && this.currentRound.length === 1 && !this.left && !this.right) {
+        this.left = this.currentRound.pop()
+        this.finishFlag = true
+      }
+    }
   },
   created() {
     this.fetchMovies()
